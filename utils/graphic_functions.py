b53c6be9cf57f4bc4e08faf1075cf052087962ca
InwocK <80909564+InwocK@users.noreply.github.com>
2022-01-30 16:55:38 +0100

    Add files via upload

--- utils/graphic_functions.py
+++ utils/graphic_functions.py
@@ -1,4 +1,8 @@
-import pygame, sys
+import pygame, sys, json
+import utils.player_functions
+import utils.game_functions
+import utils.items_functions
+import utils.map_functions
 
 def setup():
     try:
@@ -6,22 +10,28 @@ def setup():
         clock = pygame.time.Clock()
     except Exception as e:
         print(e)
+    return clock
 
 #create the display surface
 def display(game):
     try:
         pygame.display.set_caption(game["player"]["name"])
-
+        clock = setup()
         #surface principal
         screen = pygame.display.set_mode((1310,710))
 
+        if game["player"]["location"]["detail"] == "":
+            map_surface = pygame.Surface((990,490))
+            map_surface.fill((20,148,20))
+        elif game["player"]["location"]["detail"] == "Lindenvale.inn.lobby":
+            map_surface = pygame.Surface((990,490)) and pygame.image.load("img/hall_inn_innkeeper.jpg")
+        else:
+            map_surface = pygame.Surface((990,490)) and pygame.image.load("img/cities.jpg")        
 
-        #surface second
-        map_surface = pygame.Surface((990,490))
-        map_surface.fill((80,186,203))
+        choice_surface = pygame.Surface((990,190)) and pygame.image.load("img/choice_background.jpg")
+
         inventory_surface = pygame.Surface((290,690))
         inventory_surface.fill((137,137,137))
-        choice_surface = pygame.Surface((990,190)) and pygame.image.load("img/choice_background.jpg")
 
         while True:
             for event in pygame.event.get():
@@ -36,26 +46,9 @@ def display(game):
         
             pygame.display.flip()
             clock.tick(60)
+
     except Exception as e:
         print(e)
-
-#def update_map(game):
-    #try:
-
-    #except Exception as e:
-        #print(e)
-
-#def update_choice(choice):
-    #try:
-
-    #except Exception as e:
-        #print(e)
-
-#def update_inventory(game):
-    #try:
-
-    #except Exception as e:
-        #print(e)
 
 #def menu(game):
     #try:
